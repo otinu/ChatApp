ee40559bd139fbbe710b9e8a700574b447183882
otinu <fitshter@gmail.com>
2024-10-20 20:21:10 +0900

    10-38

---
+++ ChatViewWithSwiftUI/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  ChatViewWithSwiftUI
+//
+//  Created by Apple on 2024/10/20.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
