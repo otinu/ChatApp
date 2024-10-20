78948bc0ba6ce48939956ae6d26eb69f46a80ef0
otinu <fitshter@gmail.com>
2024-10-20 11:38:30 +0900

    6-21

---
+++ ChatViewWithSwiftUI/Models/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  ChatViewWithSwiftUI
+//
+//  Created by Apple on 2024/10/20.
+//
+
+import Foundation
+
+struct Message {
+    // IDを自動採番
+    let id: String = UUID().uuidString
+    let text: String
+    let user: User
+    let date: Date
+    let alreadyRead: Bool
+}
--- ChatViewWithSwiftUI/Views/DataModelTest.swift
+++ ChatViewWithSwiftUI/Views/DataModelTest.swift
@@ -12,13 +12,24 @@ struct DataModelTest: View {
     let user1 = User(id: "1", name: "John", image: "user01")
     let user2 = User(id: "2", name: "Key", image: "user02")
     
+    let message = Message(text: "Hello",
+                          user: User(id: "1", name: "John", image: "user01"),
+                          date: Date(),
+                          alreadyRead: false)
+    
     var body: some View {
         VStack {
+            Text(message.text)
+            Text(message.user.name)
+            
             Text(user1.name)
-            Image(user1.image)
+            // Image(user1.image)
             
             Text(user2.name)
-            Image(user2.image)
+            // Image(user2.image)
+            
+            
+            
         }
     }
 }
