74e0c1f60a8de9dc050e109391885364fac74b7e
otinu <fitshter@gmail.com>
2024-10-20 10:34:24 +0900

    5-17

--- ChatViewWithSwiftUI/Views/MessageRow.swift
+++ ChatViewWithSwiftUI/Views/MessageRow.swift
@@ -9,16 +9,37 @@ import SwiftUI
 
 struct MessageRow: View {
     var body: some View {
-        HStack {
-            Circle()
-                .frame(width: 60, height: 60)
-            Capsule()
-                .frame(height: 60)
+        HStack(alignment: .top) {
+            Image(systemName: "person.circle")
+                .resizable()
+                .frame(width: 48, height: 48)
+            Text("Hello World")
+                .padding()
+                .background(.white)
+                .cornerRadius(30)
+            VStack(alignment: .trailing) {
+                // 既読を右下に表示させるためにSpacer()を設置。
+                // プレビューでは画面全体の右下でも、シミュレータを起動するとキレイに配置される
+                Spacer()
+                Text("既読")
+                // 現在時刻の表示
+                Text(formattedDataString)
+            }
+            .foregroundColor(.secondary)
+            .font(.footnote)
+            Spacer()
         }
         .padding(.bottom)
+    }
+    
+    private var formattedDataString: String {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter.string(from: Date())
     }
 }
 
 #Preview {
     MessageRow()
+        .background(.cyan)
 }
