fe40c12c6aa101de943fb6d4a4bd0a6e8c590f86
otinu <fitshter@gmail.com>
2024-10-25 22:43:12 +0900

    14-53

--- ChatViewWithSwiftUI/Views/ChatView.swift
+++ ChatViewWithSwiftUI/Views/ChatView.swift
@@ -82,6 +82,10 @@ extension ChatView {
             .onChange(of: chat.messages) {
                 scrollToLast(proxy: proxy, smooth: true)
             }
+            
+            .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardDidShowNotification)) { _ in
+                scrollToLast(proxy: proxy, smooth: true)
+            }
         }
     }
     
